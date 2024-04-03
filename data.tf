8c2257078d0370dcd1dc772f4cb224f5ca442e7d
Steven Fram <steve@monami.io>
2024-04-03 10:07:15 -0700

    remove comment

--- data.tf
+++ data.tf
@@ -1,7 +1,6 @@
 data "aws_ami" "amazon-linux" {
   most_recent = true
   owners      = ["amazon"]
-#  name_regex  = "al2023-ami-kernel-default-x86_64"
   name_regex  = "al2023-ami-minimal.*"
 
   filter {
